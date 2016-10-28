552ae15cd580c8e867bcc5ea564a6443d7e7c851
Grace Durham <gracelee.durham@gmail.com>
2016-10-27 21:12:29 -0700

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,55 @@
+# print range(5)
+# # print range(3.14)
+# print range(-8)
+
+# print range(0)
+
+# print range(1)
+# print range (1,5)
+# print range(5,1)
+# print range(5, 1, -1)
+
+# for item in range(1, 21):
+#     print item,
+
+# for item in range(1, 21):
+#     if item == 13:
+#         print "hello" 
+#     else:
+#         print item,
+
+# fruits = ["apples", "oranges", "bananas"]
+
+# for fruit in fruits:
+#     print fruit, 
+
+
+ # for fruit in range(len(fruits)):
+ #     print fruits[fruit]
+
+
+count = 0
+while (count < 21):
+    print count,
+    count += 1
+
+
+count = 0
+while (count < 21):
+    if count == 13:
+        print "hello"
+    else:    
+        print count,
+    count += 1
+
+count = 0
+while (count < 101):
+    print count,
+    count += 10    
+
+fruits= ["apples", 'oranges', 'bananas']
+
+index=0
+while(index<=2):
+    print fruits[index]
+    index=index + 1
